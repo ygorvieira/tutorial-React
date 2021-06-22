17ca3764377f73cbe9ad99e67588a8af45e02f26
Ygor Vieira <ygorus@gmail.com>
2021-06-22 13:43:47 -0300

    Renderizando listas

--- src/components/contador.jsx
+++ src/components/contador.jsx
@@ -1,7 +1,9 @@
 import React, {Component} from "react";
 
 class Contador extends Component{
-    state = { contador: 0 };
+    state = { contador: 0, 
+        itens: ['item1', 'item2', 'item3']
+    };
 
     estilos = {
         fontSize: 25,
@@ -9,16 +11,18 @@ class Contador extends Component{
   };
 
     render() {    
-        let classes = "badge m-2 badge-";
-        classes += (this.state.contador > 10 ? "success" : "primary")
+        
         return (
         <React.Fragment>
-            <span style={this.estilos} className={classes}>
+            <span style={this.estilos} className={this.GetBadgeClasses()}>
             {this.formataContador()}
             </span>
             <button onClick={this.tratarIncremento} className="btn btn-secondary">
             Incrementar
             </button>
+            <ul>
+                {this.state.itens.map(item => <li key = {item}>{item}</li>)}
+            </ul>
         </React.Fragment>
         );
     }
@@ -31,6 +35,12 @@ class Contador extends Component{
     tratarIncremento = () => {
         this.setState({ contador: this.state.contador + 1 });
     };
+
+    GetBadgeClasses(){
+        let classes = "badge m-2 badge-";
+        classes += (this.state.contador > 10 ? "success" : "primary");
+        return classes;
+    }
 }
 
 export default Contador;
